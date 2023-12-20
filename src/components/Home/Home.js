198d83abcbdf66c6ae8ce73330109fa4ca4b7b55
Sulaimon Ayodeji <sulaimonaa@gmail.com>
2023-12-20 13:22:30 +0100

    Update Home.js
    Fix the name error on the popular component import

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header1 from '../Navigation/Header1'
 import HeroBanner from '../Hero'
-import PopularFlights from '../Popular-flights'
+import PopularFlights from '../popular-flights'
 
 const Home = () => {
   return (
